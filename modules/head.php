8943b21253639a8eead61b30a318e60db5cd0610
Mecham-Timothy <tmecham@gmail.com>
2014-07-08 17:38:26 -0600

    Updated CSS for phone largdevice added teez logo and updated head.php so as to use the new CSS settings.

--- modules/head.php
+++ modules/head.php
@@ -1,11 +1,12 @@
-
-
 /* 
 * To change this license header, choose License Headers in Project Properties.
 * To change this template file, choose Tools | Templates
 * and open the template in the editor.
 */
-
+<meta charset="UTF-8">
+<meta content="width=device-width" name="viewport">
+<link href="/css/phone.css" type="text/css" rel="stylesheet" media="screen">
+<link href="/css/largdevice.css" type="text/css" rel="stylesheet" media="screen">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jquerymobile/1.4.3/jquery.mobile.min.css" />
 <script src="//ajax.googleapis.com/ajax/libs/jquerymobile/1.4.3/jquery.mobile.min.js"></script>
