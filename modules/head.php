e8feffd7bfd21938b6e91635979c18777e4ac011
Mecham-Timothy <tmecham@gmail.com>
2014-07-08 17:52:39 -0600

    Updated head to remove commenting.

--- modules/head.php
+++ modules/head.php
@@ -1,8 +1,3 @@
-/* 
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
-*/
 <meta charset="UTF-8">
 <meta content="width=device-width" name="viewport">
 <link href="/css/phone.css" type="text/css" rel="stylesheet" media="screen">
