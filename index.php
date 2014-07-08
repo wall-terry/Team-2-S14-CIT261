b2158ed0fb72c231994df1248842046404e4f85b
Terry Wall <twall411@roadrunner.com>
2014-07-08 16:31:40 -0700

    added Javascript headers for Jquery and Jquery UI  added session code.

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 
 <?php
 
+/*start a new session or resume the previous session
+ * 
+ */
+$lifetime = 60 * 60 *  24 * 14;
+session_set_cookie_params($lifetime, '/');
+session_start();
+
+/*
+ * Check if this function is being called from a post or get action by checking
+ * for the presence of post or get data action flags and
+ * set the action flag to 'home' if this is being called as the Apache default page.
+ */
+
 if (isset($_POST['action'])) {
     $action = $_POST['action'];
 } else {
@@ -13,6 +26,9 @@ if (isset($_POST['action'])) {
 
     switch ($action) {
         case 'home':
+            if (empty($_SESSION['userID'])){
+                
+            }
             include('views/pageviews.php');
             break;
 
--- modules/head.php
+++ modules/head.php
@@ -1,8 +1,13 @@
-<?php
+
 
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jquerymobile/1.4.3/jquery.mobile.min.css" />
+<script src="//ajax.googleapis.com/ajax/libs/jquerymobile/1.4.3/jquery.mobile.min.js"></script>
+<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.0/themes/smoothness/jquery-ui.css" />
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.0/jquery-ui.min.js"></script>
