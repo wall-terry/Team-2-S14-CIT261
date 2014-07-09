a6c2f1125a308fb4a7722b759fba647418de40ba
Terry Wall <twall411@roadrunner.com>
2014-07-09 09:45:32 -0700

    Created sql script to create the database.

--- model/database.php
+++ model/database.php
@@ -12,3 +12,23 @@ class Database {
     private $password = 'CIT261s14t#2';
     
 }
+
+function get_name_by_userID($userID){  
+    $query = 'SELECT  first_name, lastname FROM users
+            WHERE userID = $userID';           
+}
+
+function build_database(){
+  $query = 'CREATE TABLE [IF NOT EXISTS] users 
+          { userID      INT     NOT NULL    UNIQUE,
+            username    VARCHAR(60) NOT NULL,
+            email       VARCHAR(255) NOT NULL,
+            password    VARCHAR(60) NOT NULL,
+            verified    BOOLEAN(60) NOT NULL,
+            role        VARCHAR(15) NOT NULL,
+            address     INT(11),
+            first_name  VARCHAR(60),
+            last_name   VARCHAR(60),
+            
+            }';
+}
--- modules/head.php
+++ modules/head.php
@@ -1,10 +1,4 @@
-/* 
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
-*/
-<meta charset="UTF-8">
-<meta content="width=device-width" name="viewport">
+
 <link href="/css/phone.css" type="text/css" rel="stylesheet" media="screen">
 <link href="/css/largdevice.css" type="text/css" rel="stylesheet" media="screen">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
