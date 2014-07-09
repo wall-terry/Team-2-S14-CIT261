edafbb85df5d16390a28ad01368fa9da18032a3d
Terry Wall <twall411@roadrunner.com>
2014-07-09 09:52:04 -0700

    Removed JQuery Stylesheets

--- modules/head.php
+++ modules/head.php
@@ -2,7 +2,5 @@
 <link href="/css/phone.css" type="text/css" rel="stylesheet" media="screen">
 <link href="/css/largdevice.css" type="text/css" rel="stylesheet" media="screen">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jquerymobile/1.4.3/jquery.mobile.min.css" />
 <script src="//ajax.googleapis.com/ajax/libs/jquerymobile/1.4.3/jquery.mobile.min.js"></script>
-<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.0/themes/smoothness/jquery-ui.css" />
 <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.0/jquery-ui.min.js"></script>
